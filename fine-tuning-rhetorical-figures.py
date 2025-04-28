12e47535a0f67b62ccfb8a6b3f0b095b0a197775
Pier Balestrucci <pierbale97@gmail.com>
2025-04-28 16:50:47 +0200

    new configuration for epito

--- fine-tuning-rhetorical-figures.py
+++ fine-tuning-rhetorical-figures.py
@@ -1,202 +1,115 @@
-#!pip install -q accelerate
-#!pip install peft
-#!pip install bitsandbytes
-#!pip install transformers
-#!pip install trl
-#!pip install huggingface_hub
-#!pip install accelerate
-#!pip install --upgrade torch
+# Installazioni necessarie (da eseguire solo una volta)
+# pip install peft transformers trl huggingface_hub accelerate --upgrade torch
 
 import os
 import torch
-from datasets import load_dataset
+import gc
+import pandas as pd
+import re
+from datasets import Dataset
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
-    BitsAndBytesConfig,
-    HfArgumentParser,
     TrainingArguments,
     pipeline,
-    logging,
 )
 from peft import LoraConfig, PeftModel
-from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
-
-import pandas as pd
-from datasets import Dataset
-import re
-import random
-import gc
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from huggingface_hub import login
 
-print("Librerie installate correttamente")
+# Configurazione gestione memoria PyTorch
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+torch.set_default_dtype(torch.bfloat16 if torch.cuda.is_available() else torch.float32)
 
-# hugging face
+# Login HuggingFace
 login(token="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")
 
-# Paths for datasets and models
-data_path_datasets = '.\\datasets\\twittiro'
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
 
-data_path_models = '.\\models\\rhetorical-figures'
-if not os.path.exists(data_path_models):
-    os.makedirs(data_path_models)
+# Paths
+data_path_datasets = './datasets/twittiro'
+data_path_models = './models/rhetorical-figures'
+os.makedirs(data_path_models, exist_ok=True)
 
-models_name = {
-    'mistralai/Ministral-8B-Instruct-2410', # https://huggingface.co/mistralai/Ministral-8B-Instruct-2410
-    'meta-llama/Llama-3.1-8B-Instruct', # https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
-    'swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA', #https://huggingface.co/swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA
-    'Qwen/Qwen2.5-7B-Instruct', # https://huggingface.co/Qwen/Qwen2.5-7B-Instruct
-    'sapienzanlp/Minerva-7B-instruct-v1.0', # https://huggingface.co/sapienzanlp/Minerva-7B-instruct-v1.0
-}
+models_name = [
+    'mistralai/Ministral-8B-Instruct-2410',
+    'meta-llama/Llama-3.1-8B-Instruct',
+    'swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA',
+    'Qwen/Qwen2.5-7B-Instruct',
+    'sapienzanlp/Minerva-7B-instruct-v1.0',
+]
 
-################################################################################
-# QLoRA parameters
-################################################################################
-lora_r = 64 # LoRA attention dimension
-lora_alpha = 16 # Alpha parameter for LoRA scaling
-lora_dropout = 0.1 # Dropout probability for LoRA layers
-
-################################################################################
-# bitsandbytes parameters
-################################################################################
-use_4bit = True # Activate 4-bit precision base model loading
-bnb_4bit_compute_dtype = "float16" # Compute dtype for 4-bit base models
-bnb_4bit_quant_type = "nf4" # Quantization type (fp4 or nf4)
-use_nested_quant = False # Activate nested quantization for 4-bit base models (double quantization)
-
-################################################################################
-# TrainingArguments parameters
-################################################################################
-num_train_epochs = 5 # Number of training epochs
-output_dir = "./results" # Output directory where the model predictions and checkpoints will be stored
-fp16 = False # Enable fp16 training
-bf16 = True # Enable bf16 training (set bf16 to True with an A100)
-gradient_accumulation_steps = 1 # Number of update steps to accumulate the gradients for
-gradient_checkpointing = True # Enable gradient checkpointing
-max_grad_norm = 0.3 # Maximum gradient normal (gradient clipping)
-learning_rate = 2e-4 # Initial learning rate (AdamW optimizer)
-weight_decay = 0.001 # Weight decay to apply to all layers except bias/LayerNorm weights
-optim = "paged_adamw_32bit" # Optimizer to use
-lr_scheduler_type = "cosine" # Learning rate schedule
-max_steps = -1 # Number of training steps (overrides num_train_epochs)
-warmup_ratio = 0.03 # Ratio of steps for a linear warmup (from 0 to learning rate)
-group_by_length = True # Group sequences into batches with same length. Saves memory and speeds up training considerably
-save_steps = 0 # Save checkpoint every X updates steps
-logging_steps = 25 # Log every X updates steps
-batch_size = 32
-
-################################################################################
-# SFT parameters
-################################################################################
-max_seq_length = None # Maximum sequence length to use
-packing = False # Pack multiple short examples in the same input sequence to increase efficiency
-device_map = {"": 0} # Load the entire model on the GPU 0
-
-print("Inizializzazione completata")
-
-def prepare_dataset(split):
-    """Prepares dataset for the specified language and split."""
-    print(f"Preparing {data_path_datasets}\\{split}.csv dataset...")
-    df = pd.read_csv(f'{data_path_datasets}\\{split}.csv')
-    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-
-    dataset = []
-    for index, row in df.iterrows():
-        dataset.append({
-            'instruction': f'Given the ironic sentence (INPUT), identify and return the rhetorical figure it exemplifies in (OUTPUT).',
-            'id': row['id'],
-            'input': row['input'],
-            'output': row['output'],
-        })
-
-    return Dataset.from_pandas(pd.DataFrame(dataset, columns=['instruction', 'id', 'input', 'output']))
-
-
-# Load tokenizer and model with QLoRA configuration
-compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=use_4bit,
-    bnb_4bit_quant_type=bnb_4bit_quant_type,
-    bnb_4bit_compute_dtype=compute_dtype,
-    bnb_4bit_use_double_quant=use_nested_quant,
-)
-
-# Check GPU compatibility with bfloat16
-if torch.cuda.is_available() and compute_dtype == torch.float16 and use_4bit:
-    major, _ = torch.cuda.get_device_capability()
-    if major >= 8:
-        print("=" * 80)
-        print("Your GPU supports bfloat16: accelerate training with bf16=True")
-        print("=" * 80)
-
-def load_model(model_name):
-    # Load base model
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config=bnb_config,
-        device_map=device_map
-    )
-    model.config.use_cache = False
-    model.config.pretraining_tp = 1
-
-    # Load tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
-
-    return model, tokenizer
-
-# Load LoRA configuration
+# LoRA parameters
 peft_config = LoraConfig(
-    lora_alpha=lora_alpha,
-    lora_dropout=lora_dropout,
-    r=lora_r,
+    lora_alpha=16,
+    lora_dropout=0.1,
+    r=64,
     bias="none",
     task_type="CAUSAL_LM",
 )
 
-# Set training parameters
+# TrainingArguments parameters
 training_arguments = TrainingArguments(
-    output_dir=output_dir,
-    num_train_epochs=num_train_epochs,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    optim=optim,
+    output_dir="./results",
+    num_train_epochs=5,
+    per_device_train_batch_size=1,  # Ridotto
+    per_device_eval_batch_size=1,
+    gradient_accumulation_steps=1,
+    optim="adamw_torch",
     eval_strategy="steps",  # Calcolo della validation loss ad ogni step
-    save_strategy="steps",  # Checkpoint del modello ad ogni step
-    save_total_limit=3,  # Limita il numero di checkpoint salvati
-    load_best_model_at_end=True,  # Carica il modello migliore al termine dell'addestramento
-    metric_for_best_model="eval_loss",  # Sceglie la metrica per determinare il miglior modello
-    greater_is_better=False,  # Indica se un valore più alto della metrica è migliore o no
+    save_strategy="no",
+    learning_rate=2e-4,
+    weight_decay=0.001,
     eval_steps=500,  # Numero di passaggi prima di valutare il modello
     logging_steps= 500,
-    learning_rate=learning_rate,
-    weight_decay=weight_decay,
-    fp16=fp16,
-    bf16=bf16,
-    max_grad_norm=max_grad_norm,
-    max_steps=max_steps,
-    warmup_ratio=warmup_ratio,
-    group_by_length=group_by_length,
-    lr_scheduler_type=lr_scheduler_type,
+    fp16=False,
+    bf16=True,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="cosine",
     report_to="tensorboard"
 )
 
+print("Inizializzazione completata")
+
+def prepare_dataset(split):
+    print(f"Preparing {data_path_datasets}/{split}.csv dataset...")
+    df = pd.read_csv(f'{data_path_datasets}/{split}.csv')
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    dataset = [{
+        'instruction': 'Given the ironic sentence (INPUT), identify and return the rhetorical figure it exemplifies in (OUTPUT).',
+        'id': row['id'],
+        'input': row['input'],
+        'output': row['output'],
+    } for _, row in df.iterrows()]
+
+    return Dataset.from_pandas(pd.DataFrame(dataset))
+
+def load_model(model_name):
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        device_map="auto"
+    )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    return model, tokenizer
+
 def formatting_prompts_func(example):
-    output_texts = []
-    for i in range(len(example['instruction'])):
-        text = f'''<s> [INST] {example['instruction'][i]} [/INST] [INPUT] {example['input'][i]} [/INPUT] [OUTPUT] {example['output'][i]} [/OUTPUT] </s>'''
-
-        output_texts.append(text)
-
-    return output_texts
+    return [f'''<s> [INST] {instr} [/INST] [INPUT] {inp} [/INPUT] [OUTPUT] {outp} [/OUTPUT] </s>'''
+            for instr, inp, outp in zip(example['instruction'], example['input'], example['output'])]
 
 def train(model, tokenizer, train_dataset, eval_dataset):
-    response_template = " [OUTPUT]"
-    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
-
+    collator = DataCollatorForCompletionOnlyLM(" [OUTPUT]", tokenizer=tokenizer)
     trainer = SFTTrainer(
         model=model,
         train_dataset=train_dataset,
@@ -206,110 +119,90 @@ def train(model, tokenizer, train_dataset, eval_dataset):
         formatting_func=formatting_prompts_func,
         data_collator=collator,
     )
-
-    # Train model
     trainer.train()
-
     return trainer
 
 def load_fine_tuned_model(model_name, fine_tuned_model_path):
-  # Reload model in FP16 and merge it with LoRA weights
-  base_model = AutoModelForCausalLM.from_pretrained(
-      model_name,
-      low_cpu_mem_usage=True,
-      return_dict=True,
-      torch_dtype=torch.float16,
-      device_map=device_map,
-  )
+    base_model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        device_map="auto"
+    )
+    model = PeftModel.from_pretrained(base_model, fine_tuned_model_path)
+    model = model.merge_and_unload()
 
-  model = PeftModel.from_pretrained(base_model, fine_tuned_model_path)
-  model = model.merge_and_unload()
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
 
-  # Reload tokenizer to save it
-  tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-  tokenizer.pad_token = tokenizer.eos_token
-  tokenizer.padding_side = "right"
-
-  return model, tokenizer
+    return model, tokenizer
 
 def clear_output(output):
     match = re.search(r'\[OUTPUT\](.*?)\[/OUTPUT\]', output, re.DOTALL)
-    return match.group(1) if match else "Output not found"
+    return match.group(1).strip() if match else "Output not found"
 
 def model_generation(pipe, test_dataset, output_path):
-    ids = []
-    inputs = []
-    predictions = []
-    actuals = []
-    generations = []
+    records = []
+    with torch.no_grad():
+        for i, record in enumerate(test_dataset, 1):
+            print(f'record #{i}')
+            result = pipe(f"<s> [INST] {record['instruction']} [/INST] [INPUT] {record['input']} [/INPUT] [OUTPUT]")
+            prediction = clear_output(result[0]['generated_text'])
 
-    i = 1
-    for record in test_dataset:
-        print(f'record #{i}')
+            records.append((record['id'], record['input'], prediction, record['output'], result[0]['generated_text']))
 
-        if not pipe:
-            max_length = int(len(f"<s> [INST] {record['instruction']} [/INST] [INPUT] {record['input']} [/INPUT] [OUTPUT] {record['output']} [/OUTPUT] </s>") * 1.5)
-            pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=max_length, temperature=0.1)
+            if i % 5 == 0:
+                gc.collect()
+                torch.cuda.empty_cache()
 
-        result = pipe(f"<s> [INST] {record['instruction']} [/INST] [INPUT] {record['input']} [/INPUT] [OUTPUT]")
-        prediction = clear_output(result[0]['generated_text'])
-
-        print(f'id: {record["id"]}')
-        print(f'input: {record["input"]}')
-        print(f'prediction: {prediction}')
-        print(f'actual: {record["output"]}')
-        print(f'generation: {result[0]["generated_text"]}')
-
-        ids.append(record['id'])
-        inputs.append(record['input'])
-        actuals.append(record['output'])
-        predictions.append(prediction)
-        generations.append(result[0]['generated_text'])
-
-        df = pd.DataFrame(list(zip(ids, inputs, predictions, actuals, generations)), columns=['ids', 'input', 'prediction', 'actual', 'generation'])
+        df = pd.DataFrame(records, columns=['ids', 'input', 'prediction', 'actual', 'generation'])
         df.to_csv(output_path, index=False)
 
-        print('\n')
-        i = i + 1
-
-
+# Caricamento dataset
 train_dataset = prepare_dataset('train')
-print(f'train len: {len(train_dataset)}')
 eval_dataset = prepare_dataset('dev')
-print(f'eval len: {len(eval_dataset)}')
 test_dataset = prepare_dataset('test')
-print(f'test len: {len(test_dataset)}')
 
 print("Dataset caricato correttamente")
 
+print("Inizio fine-tuning")
 for model_name in models_name:
+    print(f"Fine-tuning {model_name}...")
     clean_model_name = model_name.split('/')[1]
-    print(f'Model: {clean_model_name}')
-    output_path = f'{data_path_models}\\fine-tuned-{clean_model_name}'
+    output_path = f'{data_path_models}/fine-tuned-{clean_model_name}'
 
-    # Load model
     model, tokenizer = load_model(model_name)
-
-    # Train model
     trainer = train(model, tokenizer, train_dataset, eval_dataset)
-
-    # Save fine-tuned model
     trainer.save_model(f'{output_path}')
 
-    # Load fine-tuned model
+    del model, tokenizer, trainer
+    gc.collect()
+    torch.cuda.empty_cache()
+
+print("Fine-tuning completato")
+print("Inizio generazione")
+for model_name in models_name:
+    print(f"Generazione {model_name}...")
+    clean_model_name = model_name.split('/')[1]
+    output_path = f'{data_path_models}/fine-tuned-{clean_model_name}'
+
     fine_tuned_model, fine_tuned_tokenizer = load_fine_tuned_model(model_name, f'{output_path}')
 
-    # Evaluate fine-tuned model
-    pipe = pipeline('text-generation', model=fine_tuned_model, tokenizer=fine_tuned_tokenizer, max_length=500, temperature=0.1, device=0)
+    pipe = pipeline(
+        'text-generation',
+        model=fine_tuned_model,
+        tokenizer=fine_tuned_tokenizer,
+        max_length=500,
+        temperature=0.1,
+        device_map="auto",
+        torch_dtype=torch.bfloat16
+    )
+
     for i in range(3):
         model_generation(pipe, test_dataset, f'{output_path}-decoding-{i+1}.csv')
 
-    # Clear memory
-    del model
-    del tokenizer
-    del fine_tuned_model
-    del fine_tuned_tokenizer
-    del pipe
-    del trainer
+    print(f"Generazione completata per {model_name}")
+
+    del fine_tuned_model, fine_tuned_tokenizer, pipe
     gc.collect()
     torch.cuda.empty_cache()
